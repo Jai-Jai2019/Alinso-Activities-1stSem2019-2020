59f94bb1f9741cff2118b5c6e9b20e3ad6714e29
Jai-Jai2019 <56668031+Jai-Jai2019@users.noreply.github.com>
2019-11-04 12:01:13 +0800

    Create Ifstatement.java

---
+++ Midterms/Ifstatement.java
@@ -0,0 +1,21 @@
+public class Ifstatement {
+
+  public static void main (String [] args) {
+    boolean b = false;
+    if(b) {
+    System.out.println("b is true");
+      }
+      else {
+      System.out.println("b is false");
+      boolean c = true;
+    if(c) {
+    System.out.println("c is true");
+      }
+      else {
+      System.out.println("c is false");
+      
+     } 
+    }
+   }
+   }
+  
