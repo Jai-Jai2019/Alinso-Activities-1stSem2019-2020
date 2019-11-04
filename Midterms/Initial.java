e6aad495f8862ca212783d378a07c7ce445621d2
Jai-Jai2019 <56668031+Jai-Jai2019@users.noreply.github.com>
2019-11-04 12:02:09 +0800

    Create Initial.java

---
+++ Midterms/Initial.java
@@ -0,0 +1,13 @@
+public class Initial {
+
+  public static void main (String [] args) {
+
+    System.out.println("     J      A");
+    System.out.println("     J     A A");
+    System.out.println("     J    AAAAA");
+    System.out.println("J    J   A     A");
+    System.out.println("JJJJJJ  A       A");
+  
+    }
+
+}
