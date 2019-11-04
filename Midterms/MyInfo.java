e55d351e5ce1f6d70f6a6aebf348bbef64f1f170
Jai-Jai2019 <56668031+Jai-Jai2019@users.noreply.github.com>
2019-11-04 12:02:36 +0800

    Create MyInfo.java

---
+++ Midterms/MyInfo.java
@@ -0,0 +1,12 @@
+public class MyInfo {
+
+  public static void main (String [] args) {
+
+    System.out.println("Name:      ALINSO, JAIRUS KLYLE S.");
+    System.out.println("Course:    BSCS");
+    System.out.println("Year:      1");
+    System.out.println("Adress:    FE 029 Mamaga Balili La Trinidad Benguet");
+    System.out.println("Adjective: Gamer");
+
+  }
+}
